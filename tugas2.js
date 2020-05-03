c2e883b3b96f976d77774259ce5ae3be35148ebf
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 14:43:45 +0700

    TUGAS 2 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,24 @@
+
+var number = 99;
+
+var string = "gagah";
+
+var cantik = "true";
+var berat = 75;
+if(berat<65)
+	cantik = true;
+else
+	cantik = false;
+
+var kosong = "ada";
+kosong = null;
+
+
+var array = [6,7,8,9,10,11];
+
+
+console.log(number);
+console.log(string);
+console.log(cantik);
+console.log(kosong);
+console.log(array[5]);
